d43eb8d9472272f0fa13dea2f921fa9033099c1f
Stewart Kwok <stewart.kwok1@gmail.com>
2023-08-22 20:59:32 -0700

    Add mating behavior

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ cam = Camera()
 
 running = True
 
-cheetahs = [Cheetah(random.randint(20, screen.WORLDWIDTH-20), random.randint(20, screen.WORLDHEIGHT-20), 1, cam) for i in range(6)]
-impalas = [Impala(random.randint(20, screen.WORLDWIDTH-20), random.randint(20, screen.WORLDHEIGHT-20), 1, cam) for i in range(100)]
+cheetahs = [Cheetah(random.randint(20, screen.WORLDWIDTH-20), random.randint(20, screen.WORLDHEIGHT-20), 1, cam) for i in range(10)]
+impalas = [Impala(random.randint(20, screen.WORLDWIDTH-20), random.randint(20, screen.WORLDHEIGHT-20), 1, cam) for i in range(200)]
 
 while running:
     for event in pygame.event.get():
@@ -34,14 +34,22 @@ while running:
             if len(impalas) > 0 and cheetah.canSee(impala):
                 cheetah.hunt(impala)
         for c in cheetahs:
-            if c != cheetah and cheetah.collision(c) and cheetah.breedHetero(c):
+            if c == cheetah:
+                continue
+            if cheetah.collision(c) and cheetah.breedHetero(c):
                 cheetahs.append(Cheetah(cheetah.x, cheetah.y, 1, cam))
+            if cheetah.canSee(c) and cheetah.ticksSinceMate >= cheetah.ticksToMate and c.ticksSinceMate >= c.ticksToMate:
+                cheetah.angle = math.atan2(cheetah.y-c.y, c.x-cheetah.x)
     for impala in impalas:
         if impala.dead:
             impalas.remove(impala)
         for i in impalas:
-            if i != impala and impala.collision(i) and impala.breedHetero(i):
+            if i == impala:
+                continue
+            if impala.collision(i) and impala.breedHetero(i):
                 impalas.append(Impala(impala.x, impala.y, 1, cam))
+            if impala.canSee(i) and impala.ticksSinceMate >= impala.ticksToMate and i.ticksSinceMate >= i.ticksToMate:
+                impala.angle = math.atan2(impala.y-i.y, i.x-impala.x)
         impala.tick()
         impala.render(window)
     font = pygame.font.SysFont("Arial", 36)
